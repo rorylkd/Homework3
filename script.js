052771c97df632fa4a8941144d321dcc13581f45
rorylkd <rory.donaldson14@gmail.com>
2021-03-21 23:38:57 +0800

    Added function that opens a window when user clicks the generate button. Added CSS for the window.

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+function makeAWindow(msg, yeah, nah) {              // Function for popup box with yes/no options
+  var window = $("#alert");
+  window.find(".message").text(msg);
+  window.find(".yes,.no").unbind().click(function() {
+     window.hide();
+  });
+  window.find(".yes").click(yeah);
+  window.find(".no").click(nah);
+  window.show();
+
+}
+
+function popup () {             // Should create a window for each item in the array...but doesn't :(
+  var criteria = ["Would you like to include lowercase characters?", "Would you like to include uppercase characters?", "Would you like to include numeric characters?", "Would you like to include special characters?"];  
+    for (var i = 0; i < criteria.length; i++){
+   makeAWindow(criteria[i]);}
+   }
+
+
