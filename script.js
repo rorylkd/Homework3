fcab1ad1acf225c881f33ecd2ca249c7dda181be
rorylkd <rory.donaldson14@gmail.com>
2021-03-22 19:26:34 +0800

    Added an alert containing the relevant randomizer conditions to each branch of the popup function

--- script.js
+++ script.js
@@ -13,7 +13,10 @@
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
 
-
+var lower = -1;
+var upper = -1;
+var num = -1;
+var spec = -1;
 
 function makeAWindow(msg, yeah, nah) {              // Function for popup box with yes/no options
   var window = $("#alert");
@@ -27,68 +30,105 @@ function makeAWindow(msg, yeah, nah) {              // Function for popup box wi
 
 }
 
+function randomizer(length, chars) {  // Pseudorandom string generator with specific chartypes. 
+  var charset = '';
+  if (chars.indexOf('a') > -1) charset += 'abcdefghijklmnopqrstuvwxyz'; //Lowercase
+  if (chars.indexOf('A') > -1) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Uppercase
+  if (chars.indexOf('#') > -1) charset += '0123456789';                // Numeric
+  if (chars.indexOf('!') > -1) charset += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';  // Special chars.
+  var result = '';
+  for (var i = length; i > 0; --i) result += charset[Math.floor(Math.random() * charset.length)];
+  return result;
+}
+
 function popup () {  // Dear reader: sorry for this nightmare function. I kept getting weird results when I tried to loop things so I wrote it all out instead. It's horrible, I know.           
-  makeAWindow("Would you like to include lowercase characters?", 
+
+
+length = window.prompt("Please enter the desired length of your password.", "At least 8 and no more than 128");  
+
+makeAWindow("Would you like to include lowercase characters?", 
                 function yes() {makeAWindow("Would you like to include uppercase characters?", 
                                             function yes() {makeAWindow("Would you like to include numeric characters?",
                                                                         function yes() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("YEAH")},
-                                                                                        function no() {alert("NAH")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'aA#!'))
+                                                                                                         },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'aA#'))
+                                                                                        }
                                                                                         )},
                                                                         function no() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("YUP")},
-                                                                                        function no() {alert("NOPE")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'aA!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'aA'))
+                                                                                        }
                                                                                         )}
                                                                         )},
                                             function no() {makeAWindow("Would you like to include numeric characters?",
                                                                         function yes() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("uhhuh")},
-                                                                                        function no() {alert("nuhnah")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'a#!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'a#'))
+                                                                                        }
                                                                         )},
                                                                         function no() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("YEEEE")},
-                                                                                        function no() {alert("NOOOOOO")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'a!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'a'))
+                                                                                        }
                                                                         )}
                                                                         )} 
                                                                         )},
                 function no() {makeAWindow("Would you like to include uppercase characters?", 
                                             function yes() {makeAWindow("Would you like to include numeric characters?",
                                                                         function yes() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("yeep")},
-                                                                                        function no() {alert("noop")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'A#!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'A#'))
+                                                                                        }
                                                                         )},
                                                                         function no() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("oui")},
-                                                                                        function no() {alert("non")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, 'A!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, 'A'))
+                                                                                        }
                                                                         )}
                                                                         )},
                                             function no() {makeAWindow("Would you like to include numeric characters?",
                                                                         function yes() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("ja")},
-                                                                                        function no() {alert("nein")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, '#!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert(randomizer(length, '#'))
+                                                                                        }
                                                                         )},
                                                                         function no() {makeAWindow("Would you like to include special characters?",
-                                                                                        function yes() {alert("na'am")},
-                                                                                        function no() {alert("laa")}
+                                                                                        function yes() {
+                                                                                          alert(randomizer(length, '!'))
+                                                                                        },
+                                                                                        function no() {
+                                                                                          alert("Error: password generator needs at least one charset")
+                                                                                        }
                                                                         )}
                                                                         )} 
                                                                         )},
    );
    }
 
+   
 
-   function randomizer(length, chars) {  // Pseudorandom string generator with specific chartypes. 
-    var charset = '';
-    if (chars.indexOf('a') > -1) charset += 'abcdefghijklmnopqrstuvwxyz'; //Lowercase
-    if (chars.indexOf('A') > -1) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Uppercase
-    if (chars.indexOf('#') > -1) charset += '0123456789';                // Numeric
-    if (chars.indexOf('!') > -1) charset += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';  // Special chars.
-    var result = '';
-    for (var i = length; i > 0; --i) result += charset[Math.floor(Math.random() * charset.length)];
-    return result;
-}
-
- console.log(randomizer(16, 'aA#!'));  // Just for testing
+ // alert(randomizer(16, 'aA#!'));  // Just for testing
 
 
 
