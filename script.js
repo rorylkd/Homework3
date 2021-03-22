c2f9f9f81ab3beccf06b80d991a421f4f06ce27c
rorylkd <rory.donaldson14@gmail.com>
2021-03-22 11:26:46 +0800

    Added psuedorandom string generator that takes uppercase, lowercase, numeric, and special values

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+//  var password = generatePassword();
+//  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//  passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
 
 
 
@@ -32,5 +32,20 @@ function popup () {             // Should create a window for each item in the a
     for (var i = 0; i < criteria.length; i++){
    makeAWindow(criteria[i]);}
    }
+
+   function randomizer(length, chars) {  // Pseudorandom string generator with specific chartypes. 
+    var mask = '';
+    if (chars.indexOf('a') > -1) mask += 'abcdefghijklmnopqrstuvwxyz'; //Lowercase
+    if (chars.indexOf('A') > -1) mask += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // Uppercase
+    if (chars.indexOf('#') > -1) mask += '0123456789';                // Numeric
+    if (chars.indexOf('!') > -1) mask += '~`!@#$%^&*()_+-={}[]:";\'<>?,./|\\';  // Special chars.
+    var result = '';
+    for (var i = length; i > 0; --i) result += mask[Math.floor(Math.random() * mask.length)];
+    return result;
+}
+
+console.log(randomizer(16, 'a!'));  // Just for testing
+console.log(randomizer(2, '#aA!'));
+console.log(randomizer(100, '#!'));
 
 
